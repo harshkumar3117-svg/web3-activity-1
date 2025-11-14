d67bdbfe0f35be3970c6eed16b9029f693ebbd60
oceankesarwani <ocean.kesarwani@gmail.com>
2025-11-14 22:26:19 +0530

    Added voting contract

--- CSAI/LCI2025019/Voting.sol
+++ CSAI/LCI2025019/Voting.sol
@@ -12,28 +12,58 @@ contract Voting {
 
     mapping(address => bool) public Voted;
     address public owner;
+    bool public votingOpen = true; 
 
      constructor(){
         owner = msg.sender;
     }
 
-    modifier onlyOwner{
+    modifier onlyOwner {
         require(msg.sender==owner,"you are not the owner");
         _;
+    }
 
+    
+    modifier onlyWhenOpen {
+        require(votingOpen, "Voting is currently closed");
+        _;
     }
 
     function addCandidate(string memory _name) public {
         candidates.push(Candidate(_name, 0));
     }
 
-    function vote(uint id) public {
+    
+    function vote(uint id) public onlyWhenOpen {
         require(!Voted[msg.sender], "You have already voted");
         require(id < candidates.length, "Invalid input");
         Voted[msg.sender] = true;
         candidates[id].vote_ct++;
     }
 
+   
+    function closeVoting() public onlyOwner {
+        votingOpen = false;
+    }
+    
+   
+    function getWinner() public view returns (string memory winnerName, uint voteCount) {
+        require(!votingOpen, "Winner can be declared only when voting has been closed");
+
+        uint maxVotes = 0;
+        uint winner_id = 0;
+
+        for (uint i = 0; i < candidates.length; i++) {
+            if (candidates[i].vote_ct > maxVotes) {
+                maxVotes = candidates[i].vote_ct;
+                winner_id = i;
+            }
+        }
+        
+    
+        return (candidates[winner_id].name, candidates[winner_id].vote_ct);
+    }
+    
     function candidate_ct() public view returns (uint) {
         return candidates.length;
     }
@@ -43,6 +73,3 @@ contract Voting {
         return (candidates[_index].name, candidates[_index].vote_ct);
     }
 }
-
-
-
